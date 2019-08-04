a2a7d4814f9a35fdf2a28f16433111a4dc66c54b
vignesh <vignesh@192.168.43.240>
2019-08-04 22:39:03 +0530

    Initial commit.

---
+++ fervour-personal-app/src/main/java/com/personal/fervour/FervourPersonalAppApplication.java
@@ -0,0 +1,13 @@
+package com.personal.fervour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FervourPersonalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FervourPersonalAppApplication.class, args);
+	}
+
+}
